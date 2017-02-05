8e80b4e8aed03628df16c49487032b4f007d7e30
vshirsat <spykee007@gmail.com>
2017-02-04 21:55:55 -0800

    Create weatherbot.js

---
+++ weatherbot.js
@@ -0,0 +1,26 @@
+<script>
+		window.tcAsyncInit = function() {
+                          tc.init ({
+                          botkey: "7e7dc152-dfff-4730-a5d2-cfcb5
+                          appUISetting : '%7B%22minimisedScreen%
+                          version : 'v2'
+			});
+		};
+		(function(d, s, id){
+		var js, tjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		css = d.createElement('link');
+		css.type = "text/css";
+		css.rel = "stylesheet";
+		css.href = '//www.buildquickbots.com/botwidget/v2/demo/static/css/style.css';
+		css1 = d.createElement('link');
+		css1.type = "text/css";
+		css1.rel = "stylesheet";
+		css1.href = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.css';
+		js.src = '//www.buildquickbots.com/botwidget/v2/demo/static/js/sdk.js';
+		tjs.parentNode.insertBefore(js, tjs);
+		tjs.parentNode.insertBefore(css,tjs);
+		tjs.parentNode.insertBefore(css1,tjs);
+	}(document, 'script', 'tc-sdk'));
+	</script>
